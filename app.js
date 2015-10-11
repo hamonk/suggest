186ac59c473c3ab7245f95f088c2775647d50dd0
Kevin Hamon <kevlaouach@gmail.com>
2015-10-10 23:08:40 -0400

    add graph!

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var xml2js = require('xml2js');
 var _ = require('underscore');
 var async = require('async');
 
+app.use(express.static('public'));
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
@@ -15,8 +16,6 @@ app.get('/getsuggest/:keyword', function (req, res) {
 
     function subsequent_google_call(err, list_keywords, callback) {
 
-        var final_results = {};
-
         function simple_print(item) {
             console.log("subsequent search for " + item);
         }
@@ -24,9 +23,28 @@ app.get('/getsuggest/:keyword', function (req, res) {
         console.log('start subsequent calls for ' + list_keywords);
 
         async.map(list_keywords, call_google, function (err, results) {
-          console.log("Finished!");
-          callback(results);
+          var json_results = {};
+          var first_result = results[0];
+          json_results["name"] = first_result[0];
+          // json_results["children"] = _.map(first_result.slice(1, first_result.length),
+          //                                  function (a) {return {"name":a};});
+          json_results["children"] = _.map(results.slice(1, results.length), 
+                function (a) {
+                    return {"name" : a[0],
+                            "children" : _.map(a.slice(1, a.length), 
+                                               function(b) {
+                                                 return {'name':b};
+                                                })
+                        }
+                });
 
+          console.log("Here" + JSON.stringify(results));
+          results = _.map(results, function(a) {return ["<b>" + a[0] + "</b>", a.slice(1, a.length)]});
+          html_answer = _.map(results, function(a) {return "<li>"+a+"</li>"});
+          html_answer = "<ul>"+html_answer.join("")+"</ul>"
+
+
+          callback({'json':json_results, 'html':html_answer});
         });
     }
 
@@ -34,13 +52,14 @@ app.get('/getsuggest/:keyword', function (req, res) {
 
         var options = {host: 'suggestqueries.google.com',
                        path: '/complete/search?output=toolbar&hl=fr&q=' + encodeURIComponent(keyword)};
+        
+        var final_res = [];
 
         console.log("call google for " + keyword);
 
         var gsaReq = http.get(options, function (response) {
 
             var completeResponse = '';
-            var final_res = [];
 
             // from http://stackoverflow.com/questions/3691768/automatic-utf-8-encodeing-in-node-js-http-client
             // node js does not support iso format...
@@ -68,26 +87,38 @@ app.get('/getsuggest/:keyword', function (req, res) {
                     else {
                         var extract = _.pluck(answer['toplevel']['CompleteSuggestion'], 'suggestion');
                         var final_extract = _.map(extract, function(element) {return element[0]["$"]["data"];});
-                        console.log("value: " + final_extract[0]);
-                        console.log("callback: " + callback.name);
-                        if (callback.name != 'subsequent_google_call') {
-                            final_extract[0] = "<b>" + final_extract[0] + "</b>";
-                            final_extract = _.map(final_extract, function(element) {return "<li>" + element + "</li>"})
-                        }
+                        //console.log("value: " + final_extract[0]);
+                        //console.log("callback: " + callback.name);
 
-                        if (callback.name != 'subsequent_google_call') {
-                            final_res = final_extract.join("");
-                            final_res = "<ul>" + final_res + "</ul>";
+                        //this is the first call to google
+                        if (callback.name == 'subsequent_google_call') {
+                            final_res = final_extract;                           
                         }
                         else {
                             final_res = final_res.concat(final_extract);
+                            // console.log("key: " + final_extract[0]);
+                            // console.log("entry: " + final_extract.slice(1, final_extract.length))
+                            // console.log("Here it is: " + JSON.stringify(final_res));
+
+                            // if (final_res.hasOwnProperty("name")) {
+                            //     final_res["children"].push({"name":final_extract[0],
+                            //                                 "children":_.map(final_extract.slice(1, final_extract.length), 
+                            //                                   function(a) {return {"name" : a}})}); 
+                            // }
+                            // else {
+                            //     final_res["name"] = "test";
+                            //     final_res["children"] = [];
+                            // }                           
                         }
                     }               
                 });
                 
                 callback(null, final_res, function(a) {
-                    var html_answer = _.map(a, function(element) {return "<li>"+element+"</li>";});
-                    res.status(200).send(html_answer);
+                    //var html_answer = _.map(a, function(element) {return "<li>"+element+"</li>";});
+                    res.setHeader('Content-Type', 'application/json');
+                    //console.log(JSON.stringify(a[0]));
+                    //console.log(JSON.stringify(a[1]));
+                    res.status(200).send(a);
                 });
 
             });
